ea778dce3daa04f46121fccc329d36ea85a1ac06
Anatoly Vasilyev <anatoly.developer@gmail.com>
2016-02-12 12:29:06 +0300

    main in progress

--- ohmygut/core/main.py
+++ ohmygut/core/main.py
@@ -6,14 +6,19 @@ from ohmygut.core.tools import get_sentences
 
 def main(article_data_source, bacteria_catalog, nutrients_catalog, sentence_parser):
     articles = article_data_source.get_articles()
-    raw_sentences = ((sentence, article.path) for article in articles \
-                     for sentence in get_sentences(article.text))
+    sentences_titles_tuple = ((sentence, article.title) for article in articles \
+                              for sentence in get_sentences(article.text))
     results = []
-    for raw_sentence in raw_sentences:
-        sentence = Sentence(raw_sentence)
-        sentence.bacteria = bacteria_catalog.find(sentence.text)
-        sentence.nutrients = nutrients_catalog.find(sentence.text)
-        if not (sentence.bacteria and sentence.nutrients):
+    for sentence_text, article_title in sentences_titles_tuple:
+        bacteria = bacteria_catalog.find(sentence_text)
+        nutrients = nutrients_catalog.find(sentence_text)
+        if not (bacteria and nutrients):
             continue
-        sentence.parser_output = sentence_parser.parse_sentence(sentence.text)
+        parser_output = sentence_parser.parse_sentence(sentence_text)
+        sentence = Sentence(text=sentence_text,
+                            article_title=article_title,
+                            bacteria=bacteria,
+                            nutrients=nutrients,
+                            diseases=[],
+                            parse_result=parser_output)
         # analyser_output = analyser.analyse(sentence)
--- ohmygut/core/sentence.py
+++ ohmygut/core/sentence.py
@@ -1,7 +1,13 @@
-class Sentence(str):
-    """docstring for Sentence"""
-    def __init__(self, raw_sentence):
-        self.text = raw_sentence
+class Sentence(object):
+
+    def __init__(self, text, article_title, bacteria, nutrients, diseases, parse_result):
+        super().__init__()
+        self.parse_result = parse_result
+        self.diseases = diseases
+        self.nutrients = nutrients
+        self.bacteria = bacteria
+        self.article_title = article_title
+        self.text = text
 
     def __repr__(self):
         return self.text
