2645fbeb4dd497f5b8d941ebe31032c2e5fef6fd
Natasha <vera.odints@gmail.com>
2016-03-10 17:08:52 +0300

    Delete merge errors

--- ohmygut/core/main.py
+++ ohmygut/core/main.py
@@ -28,16 +28,11 @@ def main(article_data_source, bacteria_catalog, nutrients_catalog, diseases_cata
                                        for sentence in get_sentences(article.text))
     sentences = []
     n = 0
-<<<<<<< HEAD
-    for sentence_text, article_title in sentences_titles_tuple:
-        if n == 1000:
-            sys.exit()
-=======
+
     for sentence_text, article_title, article_journal in sentences_titles_journals_tuple:
->>>>>>> 38a0a7d8c746c12e6431e6fc03ee0e098d163f3e
         bacteria = bacteria_catalog.find(sentence_text)
         nutrients = nutrients_catalog.find(sentence_text)
-        # diseases = diseases_catalog.find(sentence_text)
+        diseases = diseases_catalog.find(sentence_text)
         diseases = []
 
         if sum(map(bool, [bacteria, nutrients, diseases])) < 2:
@@ -49,26 +44,11 @@ def main(article_data_source, bacteria_catalog, nutrients_catalog, diseases_cata
         if sum(map(bool, [bacteria, nutrients, diseases])) < 2:
             continue
 
-<<<<<<< HEAD
         print(sentence_text)
         print(bacteria)
         print(nutrients)
         print()
         n +=1
-        # parser_output = sentence_parser.parse_sentence(sentence_text)
-        # if not parser_output:
-        #     continue
-
-        # sentence = Sentence(text=sentence_text,
-        #                     article_title=article_title,
-        #                     bacteria=bacteria,
-        #                     nutrients=nutrients,
-        #                     diseases=diseases,
-        #                     parse_result=parser_output)
-        # print(sentence)
-        # sentence_analyzer.analyze(sentence)
-        print('=' * 80)
-=======
         parser_output = sentence_parser.parse_sentence(sentence_text)
         if not parser_output:
             continue
@@ -78,13 +58,12 @@ def main(article_data_source, bacteria_catalog, nutrients_catalog, diseases_cata
                             bacteria=bacteria,
                             nutrients=nutrients,
                             diseases=diseases,
-                            parse_result=parser_output,
-                            journal=article_journal)
+                            parse_result=parser_output)
         print(sentence)
         n += 1
         print("sentence № %i" % n)
         sentences.append(sentence)
->>>>>>> 38a0a7d8c746c12e6431e6fc03ee0e098d163f3e
+        print('=' * 80)
 
     data = sentences_to_data_frame(sentences)
     data.to_csv('sentences%s.csv' % (datetime.datetime.now().strftime("%H_%M_%S-%d_%m_%y")))
