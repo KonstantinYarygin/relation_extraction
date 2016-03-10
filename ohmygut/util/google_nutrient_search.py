f3626e12411e9a0db9eb126c4e07840e5ea27afb
Anatoly Vasilyev <anatoly.developer@gmail.com>
2016-03-10 15:55:14 +0300

    OMG-79

--- ohmygut/util/google_nutrient_search.py
+++ ohmygut/util/google_nutrient_search.py
@@ -11,9 +11,10 @@ import random
 
 from ohmygut.core import constants
 from ohmygut.core.catalog.nutrients_catalog import NutrientsCatalogNikogosov
+from ohmygut.util.sendmail import send_mail
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-
+output_file_name = 'filename.txt'
 
 def get_google_search_urls_html(html):
     parser = BeautifulSoup(html, 'lxml')
@@ -47,16 +48,29 @@ if __name__ == '__main__':
     nutrients = nutrients_catalog.get_simple_list()
     nutrients.sort()
     i = 1
+    messages = []
     for nutrient in nutrients:
         try:
             list_of_url = get_google_search_urls(nutrient, 100)
         except:
             error = traceback.format_exc()
             constants.logger.error(error)
-            constants.google_search_nutrient_logger.info("%i\t%s\t%s" % (i, nutrient, "ERROR"))
+            message = "%i\t%s\t%s" % (i, nutrient, "ERROR")
+            constants.google_search_nutrient_logger.info(message)
+            messages.append(message)
             continue
         for url in list_of_url:
-            constants.google_search_nutrient_logger.info("%i\t%s\t%s" % (i, nutrient, url))
+            message = "%i\t%s\t%s" % (i, nutrient, url)
+            constants.google_search_nutrient_logger.info(message)
+            messages.append(message)
+
         time.sleep(90)
         i += 1
+        if i == 3:
+            break
     constants.logger.info("finish google nutrients")
+
+    with open(output_file_name, 'w') as f:
+        f.write('\n'.join(messages))
+
+    send_mail("anatoly.developer@gmail.com", "nutrient google search", "<br/>".join(messages), files=[output_file_name])
