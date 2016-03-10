867fdfcc50a3e72aa0a30c875729cfaaffb545b3
Natasha <vera.odints@gmail.com>
2016-03-10 11:52:18 +0300

    OMG 63 Big bact catalog with strains

--- ohmygut/create_gut_bacterial_csv.py
+++ ohmygut/create_gut_bacterial_csv.py
@@ -1,6 +1,5 @@
 import os
 import re
-import subprocess
 
 import numpy as np
 import pandas as pd
@@ -9,30 +8,19 @@ FIELD_STRAIN = 'strain'
 
 from ohmygut.core.constants import FIELD_NAME, NCBI_COLS_NAMES, NCBI_NUM_NAMES, NCBI_NUM_NODES, NCBI_COLS_NODES, \
     CLASS_EXCLUSIONS, FIELD_CLASS, FIELD_ID, CHUNK_SIZE, FIELD_PARENT_ID, FIELD_RANK, RANK_EXCLUSIONS, TEMPLATE_CONTIG, \
-    TEMPLATE_SEP
+    CLASS_SCIENTIFIC, RANK_SPECIES
 
 
-def get_parent_ids(child_ids, ncbi_nodes):
+def get_parent_ids(id, ncbi_nodes, is_get_child_id=False):
     ids = np.array([])
-    parents_ids = np.array(child_ids)
+    parents_ids = np.array(id)
     while len(parents_ids) != 0:
-        ids = np.append(ids, parents_ids)
-        parents_ids = np.unique(ncbi_nodes[ncbi_nodes[FIELD_ID].isin(parents_ids)][FIELD_PARENT_ID].values)
+        if not is_get_child_id: ids = np.append(ids, parents_ids)
+        if not is_get_child_id: parents_ids = np.unique(ncbi_nodes[ncbi_nodes[FIELD_ID].isin(parents_ids)][FIELD_PARENT_ID].values)
+        if is_get_child_id:  parents_ids = np.unique(ncbi_nodes[ncbi_nodes[FIELD_PARENT_ID].isin(parents_ids)][FIELD_ID].values)
         parents_types = ncbi_nodes[ncbi_nodes[FIELD_ID].isin(parents_ids)][FIELD_RANK].values
         parents_ids = parents_ids[~np.in1d(parents_types, RANK_EXCLUSIONS)]
-    ids = np.unique(ids)
-    ranks = ncbi_nodes[ncbi_nodes[FIELD_ID].isin(ids)][FIELD_RANK].values
-    return pd.DataFrame({FIELD_ID: ids, FIELD_RANK: ranks})
-
-
-def get_children_ids(parent_ids, ncbi_nodes):
-    ids = np.array([])
-    children_ids = np.array(parent_ids)
-    while len(children_ids) != 0:
-        children_ids = np.unique(ncbi_nodes[ncbi_nodes[FIELD_PARENT_ID].isin(children_ids)][FIELD_ID].values)
-        children_types = ncbi_nodes[(ncbi_nodes[FIELD_ID].isin(children_ids))][FIELD_RANK].values
-        children_ids = children_ids[~np.in1d(children_types, RANK_EXCLUSIONS)]
-        ids = np.append(ids, children_ids)
+        if is_get_child_id: ids = np.append(ids, parents_ids)
     ids = np.unique(ids)
     ranks = ncbi_nodes[ncbi_nodes[FIELD_ID].isin(ids)][FIELD_RANK].values
     return pd.DataFrame({FIELD_ID: ids, FIELD_RANK: ranks})
@@ -51,13 +39,6 @@ def get_species_ids(bact_list, ncbi_names):
                        ].drop_duplicates(FIELD_ID)[FIELD_ID].tolist()
     gut_classes_ids.sort()
     return gut_species_ids, gut_classes_ids, unresolved_names
-
-
-def prepare_gut_bact_list_tyaht(gut_bact_list_path):
-    bact_list = pd.read_table(gut_bact_list_path, usecols=[0], names=[FIELD_NAME])
-    bact_list[FIELD_NAME] = bact_list[FIELD_NAME].apply(lambda x: re.sub(TEMPLATE_CONTIG, '', x))
-    bact_list[FIELD_NAME] = bact_list[FIELD_NAME].apply(lambda x: re.sub(TEMPLATE_SEP, '', x))
-    return bact_list
 
 
 def get_gut_bact_list(gut_bact_list_path):
@@ -82,19 +63,20 @@ def create_gut_bacterial_csv(nodes_ncbi_path, names_ncbi_path, gut_bact_list_pat
     [gut_species_ids, gut_class_ids, unresolved_names] = get_species_ids(bact_list=gut_names, ncbi_names=ncbi_names)
 
     gut_ids = gut_species_ids + gut_class_ids
-    gut_ids_table = get_parent_ids(child_ids=gut_ids, ncbi_nodes=ncbi_nodes)
-    gut_strain_ids = get_children_ids(gut_species_ids, ncbi_nodes)
+    gut_ids_table = get_parent_ids(gut_ids, ncbi_nodes)
+    gut_strain_ids = get_parent_ids(gut_species_ids, ncbi_nodes, is_get_child_id=True)
 
     gut_ids_table = pd.concat([gut_ids_table, gut_strain_ids]).drop_duplicates(FIELD_ID)
 
     gut_names = ncbi_names[ncbi_names[FIELD_ID].isin(gut_ids_table[FIELD_ID].tolist())]
     gut_names = pd.concat([gut_names,
                            pd.DataFrame({FIELD_ID: None,
-                                         FIELD_CLASS: 'scientific name',
+                                         FIELD_CLASS: CLASS_SCIENTIFIC,
                                          FIELD_NAME: unresolved_names})],
                           ignore_index=True)
+
     gut_names = pd.merge(gut_names, gut_ids_table, how='left', on=FIELD_ID, copy=False).drop_duplicates(FIELD_NAME)
-    gut_names.loc[gut_names[FIELD_RANK].isnull(), FIELD_RANK] = 'species'
+    gut_names.loc[gut_names[FIELD_RANK].isnull(), FIELD_RANK] = RANK_SPECIES
     gut_names.to_csv(output_csv_path, index=False)
 
 
