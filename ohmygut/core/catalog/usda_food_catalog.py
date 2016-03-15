06d8adbbaecb661a85d4981270e4268536c1de7c
Anatoly Vasilyev <anatoly.developer@gmail.com>
2016-03-15 18:25:11 +0300

    OMG-80 food catalog

--- ohmygut/core/catalog/usda_food_catalog.py
+++ ohmygut/core/catalog/usda_food_catalog.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from ohmygut.core.catalog.catalog import Catalog
+from ohmygut.core.hash_tree import HashTree
 
 
 class UsdaFoodCatalog(Catalog):
@@ -10,8 +11,10 @@ class UsdaFoodCatalog(Catalog):
         self.__hash_tree = None
 
     def find(self, sentence_text):
-        pass
+        sentence_text = sentence_text.lower()
+        return self.__hash_tree.search(sentence_text)
 
     def initialize(self):
         food_data_frame = pd.read_table(self.food_file_path)
-        food_data_frame.loc['word']
+        words = food_data_frame['word'].values
+        self.__hash_tree = HashTree(words)
---
+++ ohmygut/test/test_usda_food_catalog.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from ohmygut.core.catalog.usda_food_catalog import UsdaFoodCatalog
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestCase(unittest.TestCase):
+    test_data_file_name = "test_food_data.tsv"
+
+    def test_initialize(self):
+        target = UsdaFoodCatalog(os.path.join(script_dir, self.test_data_file_name))
+        target.initialize()
+
+    def test_search(self):
+        target = UsdaFoodCatalog(os.path.join(script_dir, self.test_data_file_name))
+        target.initialize()
+
+        actual1 = target.find("Whipped cream substitute is very tasty")
+        expected1 = ["whipped cream substitute"]
+
+        actual2 = target.find("It's appear to be that eggs with whipped cream substitute are very tasty")
+        expected2 = ["whipped cream substitute", "eggs"]
+
+        self.assertCountEqual(actual1, expected1)
+        self.assertCountEqual(actual2, expected2)
+
+
+if __name__ == '__main__':
+    unittest.main()
