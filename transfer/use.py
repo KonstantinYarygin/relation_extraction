3a317d508ec7b2ab14b3225b4c29c23d2ee99770
KonstantinYarygin <yarygin@phystech.edu>
2016-05-31 18:23:15 +0300

    delete transfer and disease_finder

--- transfer/use.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-with open('/home/anatoly/do/relation_extraction/transfer/model17_44_18-07_03_16.pkl', 'rb') as f:
-    model = pickle.load(f)
-# pickle.load("")
-# model = Doc2Vec()
-# model.load_word2vec_format("/home/anatoly/do/relation_extraction/transfer/doc2vec.mmodel.w2c")
-model.most_similar()
